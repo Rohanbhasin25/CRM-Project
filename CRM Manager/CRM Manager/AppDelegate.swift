8d057df2e4bcef52de9bfcb70a35403bef782b14
Rohan Bhasin <rohanbhasin03@gmail.com>
2020-06-16 20:12:29 +0530

    Added thread extension to show splash for 4 seconds

--- CRM Manager/CRM Manager/AppDelegate.swift
+++ CRM Manager/CRM Manager/AppDelegate.swift
@@ -11,12 +11,14 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        Thread.sleep(forTimeInterval: 4.0)
         return true
+        
     }
 
     // MARK: UISceneSession Lifecycle
@@ -26,6 +28,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+    
+   
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
