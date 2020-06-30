0c0ecf74aa026096cc632d5fa01542590eb24ba3
Rohan Bhasin <rohanbhasin03@gmail.com>
2020-06-30 23:09:54 +0530

    Added LoginView

--- CRM Manager/CRM Manager/ViewController.swift
+++ CRM Manager/CRM Manager/ViewController.swift
@@ -7,19 +7,27 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 class ViewController: UIViewController {
-
+    
+    
+@IBOutlet weak var signInButton: GIDSignInButton!
     @IBOutlet weak var LoginView: UIView!
-    @IBOutlet weak var GmailButton: UIButton!
     @IBOutlet weak var LoginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+       GIDSignIn.sharedInstance()?.presentingViewController = self
+
+            // Automatically sign in the user.
+            GIDSignIn.sharedInstance()?.restorePreviousSignIn()
+
         LoginView.layer.cornerRadius = 4.0
         LoginButton.layer.cornerRadius = 4.0
-        GmailButton.layer.cornerRadius = 4.0
+        
     }
+    
+  
 
 
 }
