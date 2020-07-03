ebb0d1ce37446ca33c757ba87964eace4563058f
Rohan Bhasin <rohanbhasin03@gmail.com>
2020-07-03 16:36:51 +0530

    Added user welcome

---
+++ CRM Manager/CRM Manager/Home_VC.swift
@@ -0,0 +1,30 @@
+//
+//  Home_VC.swift
+//  CRM Manager
+//
+//  Created by ROHAN BHASIN on 03/07/20.
+//  Copyright © 2020 ROHAN BHASIN. All rights reserved.
+//
+
+import UIKit
+
+class Home_VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CRM Manager/CRM Manager/ViewController.swift
+++ CRM Manager/CRM Manager/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import GoogleSignIn
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, GIDSignInDelegate {
     
     
 @IBOutlet weak var signInButton: GIDSignInButton!
@@ -17,13 +17,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var LoginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        GIDSignIn.sharedInstance().delegate = self
+
         GIDSignIn.sharedInstance()?.presentingViewController = self
         GIDSignIn.sharedInstance()?.restorePreviousSignIn()
        
         if ((GIDSignIn.sharedInstance()?.hasPreviousSignIn()) != nil){
             print("signed in")
-            performSegue(withIdentifier: "Trans", sender: self)
+//            performSegue(withIdentifier: "Home_VC", sender: self)
+            
+
+
         }
 
 
@@ -33,5 +37,44 @@ class ViewController: UIViewController {
 
 
 }
+    
+    // Retrieve user profile details from Google
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if (error == nil) {
+
+            // perform your log in functions here...
+            // ex. assign user profile data to variables
+            
+            print(user.profile.email as Any)
+            print(user.profile.givenName as Any)
+            print(user.profile.familyName as Any)
+       
+            
+            
+            let controller = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: "Home_VC") as! Home_VC
+            controller.modalPresentationStyle = .fullScreen // this line use from ios 13
+            self.present(controller, animated: true, completion: nil)  // use for model
+          //  self.navigationController?.pushViewController(controller, animated: true) // use for push
+            
+            
+            
+            
+            // Sign out of Google when logic completes for security purposes
+         //   GIDSignIn.sharedInstance().signOut()
+
+        } else {
+            print(error.localizedDescription)
+            
+                 
+        }
+    }
+    
+    
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            print(error.localizedDescription)
+        }
+    }
 
 }
+
