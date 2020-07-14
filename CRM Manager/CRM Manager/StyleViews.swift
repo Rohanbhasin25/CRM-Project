09d3ad5b002f0bb806f0285db94be95558404a9c
Rohan Bhasin <rohanbhasin03@gmail.com>
2020-07-14 16:18:22 +0530

    Added Login Model

---
+++ CRM Manager/CRM Manager/StyleViews.swift
@@ -0,0 +1,21 @@
+//
+//  StyleViews.swift
+//  CRM Manager
+//
+//  Created by ROHAN BHASIN on 13/07/20.
+//  Copyright © 2020 ROHAN BHASIN. All rights reserved.
+//
+
+import UIKit
+
+class StyleViews: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
